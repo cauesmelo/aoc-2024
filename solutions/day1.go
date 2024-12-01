7e02f45c520ecc670513fb8516b317c0892cc4c5
cauesmelo <cauesmelo@gmail.com>
2024-12-01 15:13:50 -0300

    Update day1.go

--- solutions/day1.go
+++ solutions/day1.go
@@ -1,7 +1,36 @@
 package solutions
 
+import (
+	"math"
+	"sort"
+
+	"github.com/cauesmelo/aoc-2024/util"
+)
+
 func (AOC) Day1_part1() int {
-	return 1
+	lines := util.GetInput(1, false)
+
+	left := make([]int, len(lines))
+	right := make([]int, len(lines))
+
+	for idx, line := range lines {
+		numbers := util.GetNumbers(line)
+
+		left[idx] = numbers[0]
+		right[idx] = numbers[1]
+	}
+
+	sort.Ints(left)
+	sort.Ints(right)
+
+	sum := 0
+
+	for idx := range lines {
+		dist := int(math.Abs(float64(left[idx] - right[idx])))
+		sum += dist
+	}
+
+	return sum
 }
 
 func (AOC) Day1_part2() int {
