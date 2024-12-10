b348e6677b23dd3a7a6893f6cbea0702c318aff7
cauesmelo <cauesmelo@gmail.com>
2024-12-10 12:07:22 -0300

    d6p2

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cauesmelo/aoc-2024
 
 go 1.23.1
+
+require golang.org/x/text v0.21.0
--- solutions/day6.go
+++ solutions/day6.go
@@ -1,6 +1,10 @@
 package solutions
 
 import (
+	"sync"
+	"sync/atomic"
+
+	benchmark "github.com/cauesmelo/aoc-2024/internal"
 	"github.com/cauesmelo/aoc-2024/util"
 )
 
@@ -106,8 +110,153 @@ func (AOC) Day6_part1() int {
 	return countPath(labPath)
 }
 
-func (AOC) Day6_part2() int {
-	// lines := util.GetInput(1, true)
+func getStartPosRune(lab [][]rune) (int, int) {
+	for i, line := range lab {
+		for j, c := range line {
+			if c == '^' {
+				return j, i
+			}
+		}
+	}
 
-	return 0
+	panic("No start position found")
+}
+
+func walkTheLabStopIfLoop(labPath [][]rune, startX int, startY int) bool {
+	dir := direction{
+		x: 0,
+		y: -1, // UP
+	}
+
+	x := startX
+	y := startY
+
+	stepCount := 0
+	lastNewStep := -1
+	pathCount := -1
+
+	for {
+		stepCount++
+
+		pc := countPath(labPath)
+
+		if pc != pathCount {
+			pathCount = pc
+			lastNewStep = stepCount
+		}
+
+		if stepCount-lastNewStep > 300 {
+			return true
+		}
+
+		destX := x + dir.x
+		destY := y + dir.y
+
+		if destX == len(labPath) || destY == len(labPath[0]) || destX < 0 || destY < 0 {
+			break
+		}
+
+		destination := labPath[destY][destX]
+
+		if destination == '#' || destination == 'O' {
+			if dir.x == -1 {
+				dir.x = 0
+				dir.y = -1
+				continue
+			}
+
+			if dir.x == 1 {
+				dir.x = 0
+				dir.y = 1
+				continue
+			}
+
+			if dir.y == 1 {
+				dir.y = 0
+				dir.x = -1
+				continue
+			}
+
+			if dir.y == -1 {
+				dir.y = 0
+				dir.x = 1
+				continue
+			}
+		} else {
+			x += dir.x
+			y += dir.y
+			labPath[y][x] = 'X'
+		}
+
+		if x == len(labPath) || y == len(labPath[0]) || x < 0 || y < 0 {
+			break
+		}
+	}
+
+	return false
+}
+
+func copyLabPath(labPath [][]rune) [][]rune {
+	newLabPath := [][]rune{}
+
+	for _, line := range labPath {
+		newLine := make([]rune, len(line))
+		copy(newLine, line)
+		newLabPath = append(newLabPath, newLine)
+	}
+
+	return newLabPath
+}
+
+func calculateLoopsPar(lines []string) int {
+	labPath := [][]rune{}
+
+	for _, line := range lines {
+		labPath = append(labPath, []rune(line))
+	}
+
+	x, y := getStartPosRune(labPath)
+
+	var loopCount int64
+	var wg sync.WaitGroup
+
+	for lineIdx, line := range labPath {
+		for chIdx := range line {
+			if x == chIdx && y == lineIdx {
+				continue
+			}
+
+			wg.Add(1)
+			chIdxCopy := chIdx
+			lineIdxCopy := lineIdx
+
+			go func() {
+				defer wg.Done()
+
+				modifiedLabPath := copyLabPath(labPath)
+				modifiedLabPath[lineIdxCopy][chIdxCopy] = 'O'
+
+				hasLoop := walkTheLabStopIfLoop(modifiedLabPath, x, y)
+
+				if hasLoop {
+					atomic.AddInt64(&loopCount, 1)
+				}
+			}()
+		}
+	}
+
+	wg.Wait()
+	return int(loopCount)
+}
+
+func (AOC) Day6_part2() int {
+	lines := util.GetInput(6, false)
+
+	b := &benchmark.Benchmark{}
+
+	b.Start("Parallel loop")
+	loops := calculateLoopsPar(lines)
+	b.EndPrint()
+
+	return loops
 }
