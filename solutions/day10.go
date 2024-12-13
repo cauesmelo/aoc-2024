e1f6a1f33e86e22eb1c41c407975829f3bb7959a
cauesmelo <cauesmelo@gmail.com>
2024-12-12 21:32:06 -0300

    d10p2

--- solutions/day10.go
+++ solutions/day10.go
@@ -6,36 +6,38 @@ import (
 	"github.com/cauesmelo/aoc-2024/util"
 )
 
-func calcPathRec(lines [][]int, curr int, x int, y int) int {
+func calcPathRec(lines [][]int, curr int, x int, y int, p2 bool) int {
 	max := len(lines[0]) - 1
 
 	if curr == 9 {
-		lines[y][x] = -1
+		if !p2 {
+			lines[y][x] = -1
+		}
 		return 1
 	}
 
 	sum := 0
 
 	if x > 0 && lines[y][x-1] == curr+1 {
-		sum += calcPathRec(lines, curr+1, x-1, y)
+		sum += calcPathRec(lines, curr+1, x-1, y, p2)
 	}
 
 	if x < max && lines[y][x+1] == curr+1 {
-		sum += calcPathRec(lines, curr+1, x+1, y)
+		sum += calcPathRec(lines, curr+1, x+1, y, p2)
 	}
 
 	if y < max && lines[y+1][x] == curr+1 {
-		sum += calcPathRec(lines, curr+1, x, y+1)
+		sum += calcPathRec(lines, curr+1, x, y+1, p2)
 	}
 
 	if y > 0 && lines[y-1][x] == curr+1 {
-		sum += calcPathRec(lines, curr+1, x, y-1)
+		sum += calcPathRec(lines, curr+1, x, y-1, p2)
 	}
 
 	return sum
 }
 
-func calcPath(lines []string) int {
+func calcPath(lines []string, p2 bool) int {
 
 	arr := make([][]int, 0)
 
@@ -58,13 +60,17 @@ func calcPath(lines []string) int {
 	for y, line := range arr {
 		for x, n := range line {
 			if n == 0 {
-				arrCp := make([][]int, len(arr))
-				for i, v := range arr {
-					arrCp[i] = make([]int, len(v))
-					copy(arrCp[i], v)
-				}
+				if p2 {
+					sum += calcPathRec(arr, 0, x, y, p2)
+				} else {
+					arrCp := make([][]int, len(arr))
+					for i, v := range arr {
+						arrCp[i] = make([]int, len(v))
+						copy(arrCp[i], v)
+					}
 
-				sum += calcPathRec(arrCp, 0, x, y)
+					sum += calcPathRec(arrCp, 0, x, y, p2)
+				}
 			}
 		}
 	}
@@ -75,11 +81,11 @@ func calcPath(lines []string) int {
 func (AOC) Day10_part1() int {
 	lines := util.GetInput(10, false)
 
-	return calcPath(lines)
+	return calcPath(lines, false)
 }
 
 func (AOC) Day10_part2() int {
-	// lines := util.GetInput(1, true)
+	lines := util.GetInput(10, false)
 
-	return 0
+	return calcPath(lines, true)
 }
