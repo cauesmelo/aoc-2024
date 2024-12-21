172e907b71666fa9d65df73bf2cff438881a2e40
Cauê Melo <cauesmelo@gmail.com>
2024-12-21 00:23:12 -0300

    d13p2

--- solutions/day13.go
+++ solutions/day13.go
@@ -8,9 +8,6 @@ import (
 	"github.com/cauesmelo/aoc-2024/util"
 )
 
-// btn A cost 3
-// btn B cost 1
-
 type clawMachineButton struct {
 	xDelta int
 	yDelta int
@@ -49,19 +46,30 @@ func parseButton(str string) clawMachineButton {
 	}
 }
 
-func parseTarget(str string) clawMachineTarget {
+func parseTarget(str string, p2 bool) clawMachineTarget {
 	re := regexp.MustCompile(`X\=(\d+),\s*Y\=(\d+)`)
 
 	matches := re.FindStringSubmatch(str)
 
-	xVal, _ := strconv.Atoi(matches[1])
-	yVal, _ := strconv.Atoi(matches[2])
+	m1 := matches[1]
+	m2 := matches[2]
 
-	return clawMachineTarget{
+	xVal, _ := strconv.Atoi(m1)
+	yVal, _ := strconv.Atoi(m2)
+
+	c := clawMachineTarget{
 		x: xVal,
 		y: yVal,
 	}
+
+	if p2 {
+		c.x = c.x + 10000000000000
+		c.y = c.y + 10000000000000
+	}
+
+	return c
 }
+
 func optimizeClaw(machine clawMachine) int {
 	minCost := 0
 
@@ -92,7 +100,7 @@ func (AOC) Day13_part1() int {
 		machines = append(machines, clawMachine{
 			a:      parseButton(lines[i]),
 			b:      parseButton(lines[i+1]),
-			target: parseTarget(lines[i+2]),
+			target: parseTarget(lines[i+2], false),
 		})
 	}
 
@@ -105,8 +113,39 @@ func (AOC) Day13_part1() int {
 	return totalCost
 }
 
-func (AOC) Day13_part2() int {
-	// lines := util.GetInput(1, true)
+func optimizeClawv2(machine clawMachine) int {
+	ca1 := float64(machine.target.x)*float64(machine.b.yDelta) - float64(machine.target.y)*float64(machine.b.xDelta)
+	ca2 := float64(machine.a.xDelta)*float64(machine.b.yDelta) - float64(machine.a.yDelta)*float64(machine.b.xDelta)
+	ca := ca1 / ca2
+	cb := (float64(machine.target.x) - float64(machine.a.xDelta)*ca) / float64(machine.b.xDelta)
+
+	isSolution := ca == float64(int(ca)) && cb == float64(int(cb))
+
+	if isSolution {
+		return int(ca)*machine.a.cost + int(cb)*machine.b.cost
+	}
 
 	return 0
+}
+
+func (AOC) Day13_part2() int {
+	lines := util.GetInput(13, false)
+
+	machines := make([]clawMachine, 0)
+
+	for i := 0; i < len(lines); i += 4 {
+		machines = append(machines, clawMachine{
+			a:      parseButton(lines[i]),
+			b:      parseButton(lines[i+1]),
+			target: parseTarget(lines[i+2], true),
+		})
+	}
+
+	totalCost := 0
+
+	for _, machine := range machines {
+		totalCost += optimizeClawv2(machine)
+	}
+
+	return totalCost
 }
